36e6d122ab0dc2ebc3559c32ccd122b337dfbfae
anhtu <tuleducanh06@gmail.com>
2024-01-04 09:25:20 +0700

    Upload source code

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.carSwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+});
+var swiper = new Swiper('.chargingSwiper', {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper('.homeSwiper', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  mousewheel: true,
+  speed: 1500,
+});
